eb23f80b37730a44ac4dd5e196ee118ddaffae6c
“DulceWhite” <dulce.a.suarez@gmail.com>
2022-02-28 17:52:14 -0600

    Initial commit.

---
+++ script.js
@@ -0,0 +1,55 @@
+//1
+console.log(6);
+
+//2
+/* Numeric
+String
+Boolean
+Null
+Undefined*/ 
+
+//3
+// + , - , * , ** , / , % 
+
+//4
+console.log(5 % 2);//1
+console.log(5 % 4);//1
+
+//5
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+//6
+console.log("Hawk" + "eye");
+
+//7
+console.log("dulce".length + "White".length);
+
+//8
+console.log("Avengers: Endgame"[5]);
+
+//BONNUS
+
+//1
+console.log("\\");
+
+//2
+console.log(5 ** 4 - 9);
+
+//3
+console.log("I".length + "AM".length % "Iron".length - "Man".length);
+
+//4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT" [0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR" [3]);
+console.log("STAR-LORD" [0]);
+
+//5
+m = 25;
+c = 4;
+u = m + c;
+console.log(u);
